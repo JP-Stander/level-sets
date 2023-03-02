bf1600855b4f4a52bdb0d8bcb43288d65d8e721d
Jean-Pierre Stander <Jean_Pierre.Stander@partner.bmw.co.za>
2023-03-02 08:53:23 +0200

    bugfix: Minor fixes on graphical model R example

--- graphical_model.R
+++ graphical_model.R
@@ -1,5 +1,6 @@
 # Sys.setenv(RETICULATE_PYTHON = "/home/qxz1djt/.local/share/virtualenvs/aip.mlops.terraform.modules-iNbkyG8C/bin/python")
 library(reticulate)
+library(igraph)
 py_config()
 setwd(paste0(getwd(),"/level-sets"))
 
@@ -8,11 +9,11 @@ source_python("graphical_model/utils.py")
 img = load_image("../mnist/img_56.jpg")#, list(10,10)
 
 gm <- graphical_model(img, TRUE)
-nodes = gm[[1]]
-edges = gm[[2]]
-spp = gm[[3]]
+nodes = gm[1]
+edges = gm[2]
+spp = gm[3]
 
-edges_matrix = matrix(unlist(edges), ncol = dim(vertices[[1]])[1], byrow = TRUE)
+edges_matrix = matrix(unlist(edges), ncol = dim(nodes[[1]])[1], byrow = TRUE)
 edges_matrix_cut = (edges_matrix > 0.01)*edges_matrix
 graph_obj = graph_from_adjacency_matrix(edges_matrix_cut)
 g  <- graph.adjacency(edges_matrix_cut, weighted=TRUE, mode="undirected")
