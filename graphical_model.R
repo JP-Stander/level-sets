b5f36686c6ffbd05375b690609311fd0dcdd9b79
JP-Stander <jeanpierre.stander@gmail.com>
2023-04-11 19:20:02 +0000

    feat: add functionality to calculate and plot by cliques/graphlets

--- graphical_model.R
+++ graphical_model.R
@@ -4,12 +4,16 @@
 library(reticulate)
 library(igraph)
 py_config()
-setwd(paste0(getwd(), "/level-sets"))
+# setwd(paste0(getwd(), "/level-sets"))
+
+for(pkg in c("pandas", "Pillow", "scikit-image", "scikit-learn", "opencv-python")){
+  py_install(pkg)  
+}
 
 source_python("level_sets/utils.py")
 source_python("graphical_model/utils.py")
 source("graphical_model/utils.R")
-img <- load_image("../mnist/img_45.jpg") # , list(10,10))
+img <- load_image("data/img_16.jpg") # , list(10,10))
 
 gm <- graphical_model(
   img,
@@ -22,12 +26,18 @@ edges <- gm[2]
 spp <- gm[3]
 
 edges_matrix <- matrix(unlist(edges), ncol = dim(nodes[[1]])[1], byrow = TRUE)
-edges_matrix_cut <- (edges_matrix > 0.5) * edges_matrix
+binary_edges <- edges_matrix > 0.5
+
+keep_idx <- which(colSums(edges_matrix > 0.5) > 1)
+edges_matrix <- edges_matrix[keep_idx, keep_idx]
+binary_edges <- binary_edges[keep_idx, keep_idx]
+
+edges_matrix_cut <- (binary_edges) * edges_matrix
 
 g <- graph.adjacency(edges_matrix_cut, weighted = TRUE, mode = "undirected")
-g <- set_vertex_attr(g, "intensity", index = V(g), spp[[1]][, 4])
+g <- set_vertex_attr(g, "intensity", index = V(g), spp[[1]][keep_idx, 4])
 
-coordinates <- as.matrix(rotate(as.data.frame(spp[[1]][, 2:3]), 90))
+coordinates <- as.matrix(rotate(as.data.frame(spp[[1]][keep_idx, 2:3]), 90))
 coordinates[is.nan(coordinates)] <- 0
 
 g <- set.vertex.attribute(g, "x_coordinate", index = V(g), coordinates[, 1])
@@ -38,6 +48,41 @@ plot(
   vertex.size = 5, # replace(spp[[1]][,5],spp[[1]][,5] %in% c(272, 102),20),
   vertex.color = gray(V(g)$intensity),
   layout = cbind(V(g)$x_coordinate, V(g)$y_coordinate)
+)
+
+graphlets <- graphlet_basis(g)
+g <- set.vertex.attribute(g, "n_graphlets", index = V(g), 0)
+for (c in 1:length(graphlets$cliques)){
+  g <- set.vertex.attribute(g, "color_code_gl", index = graphlets$cliques[[c]], c)
+  V(g)[graphlets$cliques[[c]]]$n_graphlets <- V(g)[graphlets$cliques[[c]]]$n_graphlets + 1
+}
+
+cliquess <- cliques(g)# graphlet_basis(g)
+g <- set.vertex.attribute(g, "n_cliques", index = V(g), 0)
+for (c in 1:length(cliquess)){
+  g <- set.vertex.attribute(g, "color_code_cl", index = cliquess[[c]], c)
+  V(g)[cliquess[[c]]]$n_cliques <- V(g)[cliquess[[c]]]$n_cliques + 1
+}
+
+plot(
+  g,
+  vertex.size = ceiling(V(g)$n_graphlets/20),
+  # vertex.size = ceiling(V(g)$n_cliques),
+  # vertex.color = V(g)$color_code_gl,
+  vertex.color = gray(V(g)$intensity),
+  # vertex.color = V(g)$color_code_cl,
+  # vertex.frame.color = V(g)$color_code_cl,
+  # layout = cbind(V(g)$x_coordinate, V(g)$y_coordinate)
+)
+
+plot(
+  g,
+  # vertex.size = ceiling(V(g)$n_graphlets),
+  vertex.size = ceiling(V(g)$n_cliques/500),
+  # vertex.color = V(g)$color_code_gl,
+  # vertex.color = V(g)$color_code_cl,
+  vertex.color = gray(V(g)$intensity),
+  # layout = cbind(V(g)$x_coordinate, V(g)$y_coordinate)
 )
 
 isolated <- which(degree(g) <= 6)
--- level_sets/metrics.py
+++ level_sets/metrics.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from skimage import measure
-from skimage.feature import greycomatrix, greycoprops
+#from skimage.feature import greycomatrix, greycoprops
 from .utils import cut_level_set
 # metrics for level-sets
 
@@ -148,47 +148,47 @@ def _pixels_to_points(pixels):
     return np.array(points).transpose()
 
 
-class GLCM:
-    # Gray-level co-occurence matrices
-    def __init__(self, image, bins=None):
-        if not bins:
-            bins = np.array([0, 16, 32, 48, 64, 80, 96, 112, 128, 144, 160, 176, 192, 208, 224, 240, 255])
-        inds = np.digitize(image, bins)
-
-        max_value = inds.max() + 1
-        matrix_coocurrence = greycomatrix(inds,
-                                          [1],
-                                          [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4],
-                                          levels=max_value,
-                                          normed=False,
-                                          symmetric=False
-                                          )
-        self.matrix_coocurrence = matrix_coocurrence
-    # GLCM properties
-
-    def contrast_feature(self):
-        contrast = greycoprops(self.matrix_coocurrence, 'contrast')
-        return contrast
-
-    def dissimilarity_feature(self):
-        dissimilarity = greycoprops(self.matrix_coocurrence, 'dissimilarity')
-        return dissimilarity
-
-    def homogeneity_feature(self):
-        homogeneity = greycoprops(self.matrix_coocurrence, 'homogeneity')
-        return homogeneity
-
-    def energy_feature(self):
-        energy = greycoprops(self.matrix_coocurrence, 'energy')
-        return energy
-
-    def correlation_feature(self):
-        correlation = greycoprops(self.matrix_coocurrence, 'correlation')
-        return correlation
-
-    def entropy_feature(self):
-        entropy = greycoprops(self.matrix_coocurrence, 'entropy')
-        return entropy
+# class GLCM:
+#     # Gray-level co-occurence matrices
+#     def __init__(self, image, bins=None):
+#         if not bins:
+#             bins = np.array([0, 16, 32, 48, 64, 80, 96, 112, 128, 144, 160, 176, 192, 208, 224, 240, 255])
+#         inds = np.digitize(image, bins)
+# 
+#         max_value = inds.max() + 1
+#         matrix_coocurrence = greycomatrix(inds,
+#                                           [1],
+#                                           [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4],
+#                                           levels=max_value,
+#                                           normed=False,
+#                                           symmetric=False
+#                                           )
+#         self.matrix_coocurrence = matrix_coocurrence
+#     # GLCM properties
+# 
+#     def contrast_feature(self):
+#         contrast = greycoprops(self.matrix_coocurrence, 'contrast')
+#         return contrast
+# 
+#     def dissimilarity_feature(self):
+#         dissimilarity = greycoprops(self.matrix_coocurrence, 'dissimilarity')
+#         return dissimilarity
+# 
+#     def homogeneity_feature(self):
+#         homogeneity = greycoprops(self.matrix_coocurrence, 'homogeneity')
+#         return homogeneity
+# 
+#     def energy_feature(self):
+#         energy = greycoprops(self.matrix_coocurrence, 'energy')
+#         return energy
+# 
+#     def correlation_feature(self):
+#         correlation = greycoprops(self.matrix_coocurrence, 'correlation')
+#         return correlation
+# 
+#     def entropy_feature(self):
+#         entropy = greycoprops(self.matrix_coocurrence, 'entropy')
+#         return entropy
 
 
 # To view points
