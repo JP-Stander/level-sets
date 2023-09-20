d4a9d89139d80cdb3f689f861bc465f8822d3ab8
Jean-Pierre Stander <Jean_Pierre.Stander@partner.bmw.co.za>
2023-09-20 22:49:39 +0200

    Modelling: Add modelling on level-sets with new metrics

---
+++ level_set_metrics_investigation.py
@@ -0,0 +1,148 @@
+# %%
+import os
+import cv2
+from matplotlib import pyplot as plt
+import seaborn as sns
+from skimage.measure import label, regionprops
+from skimage.morphology import convex_hull_image
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from level_sets.metrics import width_to_height, get_angle, compactness, elongation, perimeter
+from level_sets.utils import get_fuzzy_sets, get_level_sets
+from images.utils import load_image
+# %%
+
+def get_metrics(level_set):
+    regions = regionprops(level_set)
+
+    perim = perimeter(level_set)
+    for region in regions:
+        minr, minc, maxr, maxc = region.bbox
+        bbox_area = (maxr-minr)*(maxc-minc)
+        aspect_ratio = (maxc - minc) / (maxr - minr)
+        extent = region.area / ((maxr - minr) * (maxc - minc))
+        orientation = region.orientation
+        comp1 = region.area / (perim ** 2)
+        convex_image = convex_hull_image(region.image)
+        convex_perimeter = np.sum(convex_image)  # Approximation
+        convexity = perim / convex_perimeter
+    comp2 = compactness(level_set)
+    eln = elongation(level_set)
+    wth = width_to_height(level_set)
+    angle = get_angle(level_set)
+    return aspect_ratio, extent, eln, orientation, comp1, comp2, convexity, bbox_area, wth, angle
+
+# %%
+
+folders = ['dotted', 'fibrous']
+images = []
+for folder in folders:
+    images += [f"../dtd/images/{folder}/" + file for file in os.listdir(f"../dtd/images/{folder}")]
+
+metrics_keys = [
+    "aspect_ratio", "extent", "eln", "orientation",
+    "comp1", "comp2", "convexity", "bbox_area", "wth", "angle"
+]
+
+# Initialize metrics dictionary
+metrics = {folder: {key: [] for key in metrics_keys} for folder in folders}
+
+for image in tqdm(images):
+    img = load_image(image, [50,50])
+    level_sets = get_level_sets(img)
+    level_sets = get_fuzzy_sets(img, 10, 8)
+    uni_level_sets = pd.unique(level_sets.flatten())
+    for i, ls in enumerate(uni_level_sets):
+        subset = list(map(tuple, np.asarray(np.where(level_sets == ls)).T.tolist()))
+        if len(subset) <= 3:
+            continue
+        level_set = np.array(level_sets == ls)
+        metric_results = get_metrics(level_set.astype(int))
+        folder = image.split("/")[-2]
+        for key, result in zip(metrics_keys, metric_results):
+            metrics[folder][key].append(result)
+
+# %% Plot distributions
+data = []
+for folder, folder_metrics in metrics.items():
+    for metric_name, metric_values in folder_metrics.items():
+        for value in metric_values:
+            data.append({
+                "Folder": folder,
+                "Metric": metric_name,
+                "Value": value
+            })
+
+# Convert the list of dictionaries to a DataFrame
+df = pd.DataFrame(data)
+# Now, you can plot using seaborn
+for metric in metrics_keys:
+    plt.figure()
+    # sns.boxplot(data=df[df['Metric'] == metric], x='Folder', y=metric)
+    for folder in folders:
+        subset = df[(df['Metric'] == metric) & (df['Folder'] == folder)]
+        sns.kdeplot(subset['Value'], label=folder, fill=True)
+    plt.title(f'Distribution of {metric} across folders')
+    # plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
+# %% Mean and variance
+for folder, folder_metrics in metrics.items():
+    for metric_name, metric_values in folder_metrics.items():
+        print(f"Mean(variance) for metric {metric_name} for texture {folder}: {np.mean(metric_values)}({np.var(metric_values)})")
+
+# %% KS-statistic
+# ks_stats = pd.DataFrame(0, columns=["metric"])
+
+
+
+from scipy.stats import ks_2samp
+for metric_name in metrics_keys:
+    values1 = metrics[folders[0]][metric_name]
+    values2 = metrics[folders[1]][metric_name]
+    ks_stat, p_value = ks_2samp(values1, values2)
+    print(f"KS statistc for {metric_name}: {ks_stat} with p={p_value}")
+# %%
+metrics = {folder: {key: [] for key in metrics_keys} for folder in folders}
+
+for image in tqdm(images):
+    img = load_image(image, [50,50])
+    level_sets = get_fuzzy_sets(img, 10, 8)
+    uni_level_sets = pd.unique(level_sets.flatten())
+    for i, ls in enumerate(uni_level_sets):
+        subset = list(map(tuple, np.asarray(np.where(level_sets == ls)).T.tolist()))
+        if len(subset) <= 3:
+            continue
+        level_set = np.array(level_sets == ls)
+        metric_results = get_metrics(level_set.astype(int))
+        folder = image.split("/")[-2]
+        for key, result in zip(metrics_keys, metric_results):
+            metrics[folder][key].append(result)
+table_data = []
+
+for metric_name in metrics_keys:
+    values1 = metrics[folders[0]][metric_name]
+    values2 = metrics[folders[1]][metric_name]
+    ks_stat, p_value = ks_2samp(values1, values2)
+    table_data.append((metric_name, ks_stat))
+
+# Sort the table data by KS statistics in descending order
+table_data.sort(key=lambda x: x[1], reverse=True)
+
+# Generate LaTeX code for the table
+latex_table = "\\begin{tabular}{|c|c|}\n"
+latex_table += "\\hline\n"
+latex_table += "Metric Name & KS Statistic \\\\\n"
+latex_table += "\\hline\n"
+
+for metric_name, ks_stat in table_data:
+    latex_table += f"{metric_name} & {ks_stat:.4f} \n"
+
+latex_table += "\\hline\n"
+latex_table += "\\end{tabular}"
+
+# Print or save the LaTeX table code
+print(latex_table)
+# %%
--- proposed_method_new_mets.py
+++ proposed_method_new_mets.py
@@ -1,4 +1,16 @@
-# %%
+#%%
+import os
+import cv2
+from matplotlib import pyplot as plt
+import seaborn as sns
+from skimage.measure import label, regionprops
+from skimage.morphology import convex_hull_image
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from level_sets.metrics import width_to_height, get_angle, compactness, elongation, perimeter
+from level_sets.utils import get_fuzzy_sets, get_level_sets
+from images.utils import load_image
 from level_sets.distance_calculator import calculate_min_distance_index
 from scipy.spatial import distance_matrix
 from sklearn.base import clone
@@ -20,7 +32,29 @@ import warnings
 import xgboost as xgb
 from level_sets.metrics import width_to_height, get_angle
 warnings.simplefilter(action='ignore', category=Warning)
+
 # %%
+def get_metrics(data):
+    pixels = [a for a in eval(data.get("pixel_indices"))] if ")," in data.get("pixel_indices") else [eval(data.get("pixel_indices"))]
+    img_size = max(max(a[0] for a in pixels), max(a[1] for a in pixels))
+    level_set = np.zeros((img_size+1, img_size+1))
+    rows, cols = zip(*pixels)
+    level_set[rows, cols] = 1
+    level_set=level_set.astype(int)
+    regions = regionprops(level_set)
+
+    perim = perimeter(level_set)
+    for region in regions:
+        minr, minc, maxr, maxc = region.bbox
+        extent = region.area / ((maxr - minr) * (maxc - minc))
+        convex_image = convex_hull_image(region.image)
+        convex_perimeter = np.sum(convex_image)  # Approximation
+        convexity = perim / convex_perimeter
+    # comp = compactness(level_set)
+    # eln = elongation(level_set)
+
+    return extent, convexity #, eln, comp
+
 def image_to_histogram(descriptors, kmeans):
     hist = np.zeros(num_clusters)
     labels = kmeans.predict(descriptors)
@@ -38,21 +72,12 @@ def process_sublist(sublist_descriptors, sublist_add_features, kmeans):
     
     return full
 
-def get_wyh_and_angle(data):
-    pixels = [a for a in eval(data.get("pixel_indices"))] if ")," in data.get("pixel_indices") else [eval(data.get("pixel_indices"))]
-    img_size = max(max(a[0] for a in pixels), max(a[1] for a in pixels))
-    img = np.zeros((img_size+1, img_size+1))
-    rows, cols = zip(*pixels)
-    img[rows, cols] = 1
-    w_t_h = width_to_height(img)
-    angl = get_angle(img)
-    return w_t_h, angl
 # %%
 # Start modelling
 folders = ["dotted", "fibrous"]#os.listdir("../graphical_models_full/fuzzy_sets_10")[:10] #os.listdir("../graphical_models_full/fuzzy_sets_10")
 graph_files = [f"../graphical_models_full/fuzzy_sets_10/{folder}/{dir}" for folder in folders for dir in os.listdir(f"../graphical_models_full/fuzzy_sets_10/{folder}")]
 
-features_names = ['compactness', 'elongation', 'width_to_height', 'angle', 'intensity']
+features_names = ['compactness', 'elongation', 'extent', 'convexity', 'intensity']
 node_counts = pd.DataFrame(np.zeros((len(graph_files),len(features_names))), columns=features_names)
 
 # Read graphs
@@ -61,18 +86,14 @@ connected_subgraphs = [[] for _ in folders]
 
 for i, file in enumerate(tqdm(graph_files)):
     graph = nx.read_graphml(file)
-    clas = file.split("/")[-1].split("_")[0]
+    clas = file.split('/')[-2]
     temp_df = pd.DataFrame(np.zeros((len(graph.nodes()),len(features_names))), columns=features_names)
     for node, data in graph.nodes(data=True):
         temp_df.loc[int(node), 'compactness'] = data['compactness']
         temp_df.loc[int(node), 'elongation'] = data['elongation']
-        if 'width_to_height' in data.keys():
-            temp_df.loc[int(node), 'width_to_height'] = data['width_to_height']
-            temp_df.loc[int(node), 'angle'] = data['angle']
-        else:
-            wth, angl = get_wyh_and_angle(data)
-            temp_df.loc[int(node), 'width_to_height'] = wth
-            temp_df.loc[int(node), 'angle'] = angl
+        extent, convexity = get_metrics(data)
+        temp_df.loc[int(node), 'extent'] = extent
+        temp_df.loc[int(node), 'convexity'] = convexity
         temp_df.loc[int(node), 'intensity'] = data['intensity']
     subgraph_counts = count_unique_subgraphs(graph,4)
     sorted_sg = dict(sorted(subgraph_counts.items()))
@@ -90,12 +111,11 @@ for i, file in enumerate(tqdm(graph_files)):
     feats[loc] += [np.array(temp_df)]
     connected_subgraphs[loc] += [graphlets]
 
-
 # %%
 flattened_feats = [arr for sublist in feats for arr in sublist]
 all_descriptors = np.vstack(flattened_feats)
 
-num_clusters = 240  # (best value seems to be 240)
+num_clusters = 10  # (best value seems to be 240)
 
 # Step 1: Create a visual vocabulary
 kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(all_descriptors)
@@ -143,22 +163,52 @@ for train_index, test_index in kf.split(X):
         clf.fit(X_train, y_train)
         # print("Test accuracy:", clf.score(X_test, y_test))
         accs[name].append(clf.score(X_test, y_test))
-
 # %%
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
+for train_index, test_index in kf.split(X):
+    X_train, X_test = [X[i] for i in train_index], [X[i] for i in test_index]
+    y_train, y_test = [y[i] for i in train_index], [y[i] for i in test_index]
+    break
+bacc = 0
+for num_clusters in tqdm(range(10,250,10)):
+    kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(all_descriptors)
 
-LR = LogisticRegression(max_iter=1000, fit_intercept=False)
-LR.fit(X_train, y_train)
-print(LR.score(X_test, y_test))
+    # Step 2: Represent each image as a histogram of visual words
+    lists_of_full = [
+        process_sublist(
+            sublist_descriptors,
+            sublist_add_features,
+            kmeans
+        ) for sublist_descriptors, sublist_add_features in zip(
+            feats,
+            connected_subgraphs
+        )
+    ]
 
-# %%
-import statsmodels.api as sm
+    X = []
+    y = []
 
-# Adding a constant to the X_train to account for the intercept
-X_train_const = sm.add_constant(X_train)
-X_train_const = pd.DataFrame(X_train_const)
-model = sm.Logit(y_train, X_train_const)
-result = model.fit()
+    for class_index, sublist in enumerate(lists_of_full):
+        X.extend(sublist)
+        y.extend([class_index] * len(sublist))
 
-print(result.summary())
+    # Step 3: Train classifiers
 
+    classifiers = {
+        "Logistic Regression": LogisticRegression(max_iter=1000),
+        "Random Forest": RandomForestClassifier(n_estimators=200, random_state=42),
+        "XGBoost": xgb.XGBClassifier(objective="binary:logistic", random_state=42),
+        "SVM": SVC(),
+        "KNN": KNeighborsClassifier()
+    }
+    accs = []
+    for name, clf in classifiers.items():
+        clf.fit(X_train, y_train)
+        # print("Test accuracy:", clf.score(X_test, y_test))
+        accs.append(clf.score(X_test, y_test))
+    acc = np.max(accs)
+    if acc >= bacc:
+        bacc=acc
+        bn = num_clusters
+        print(f"Best accuracy of {bacc} with num_clusters as {num_clusters}")
 # %%
