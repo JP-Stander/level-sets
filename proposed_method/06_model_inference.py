1b31bd57047ca3b7d1b2f91e457241742ad4b9b3
Jean-Pierre Stander <Jean_Pierre.Stander@partner.bmw.co.za>
2023-10-17 22:50:05 +0200

    feat: Fix center of colourmap

--- proposed_method/06_model_inference.py
+++ proposed_method/06_model_inference.py
@@ -99,3 +99,87 @@ for clas, img_names in images_for_inference.items():
         print(f"Actual class : {clas}")
 
 # %%
+
+for clas, img_names in images_for_inference.items():
+    for img_name in img_names:
+        img_name_only = img_name.split(".")[0]
+        img_file = f"{images_loc}/{clas}/{img_name}"
+        feats = {name: [] for name in classes}
+        connected_subgraphs = {name: [] for name in classes}
+        graph = img_to_graph(
+            img_file,
+            "",
+            fs_delta,
+            img_size,
+            fs_connectivity,
+            edge_delta,
+            True,
+            sets_feature_names,
+            trim
+        )
+
+        img = load_image(img_file, [img_size, img_size], trim=trim)
+        img = img/255
+
+        coefs = {name:0 for name in graphlet_names}
+        for i, key in enumerate(coefs.keys()):
+            coefs[key] = model.coef_[0,i]
+        coefs_plane = np.zeros(img.shape)
+        nodes_attrs = []
+        
+        temp_df = pd.DataFrame(0, index=[0], columns=nodes_feature_names)
+        pred_df4 = pd.DataFrame(0, index=[0], columns = graphlet_names)
+        for node, data in graph.nodes(data=True):
+            for feature_name in nodes_feature_names:
+                temp_df.loc[int(node), feature_name] = data[feature_name]
+            temp_df.loc[int(node), "intensity"] = data["intensity"]
+            if True is True:
+                temp_df.loc[int(node), "pixel_indices"] = data["pixel_indices"]
+            g_num = f"g{kmeans.predict(temp_df.loc[int(node),nodes_feature_names].values.reshape(1,-1))[0]+1}"
+            pred_df4[g_num] += 1
+            ls_idx = [a for a in eval(data.get("pixel_indices"))] if ")," in data.get("pixel_indices") else [eval(data.get("pixel_indices"))]
+            rows, cols = zip(*ls_idx)
+            coefs_plane[rows, cols] = coefs[g_num] if g_num not in contains_zero else None
+        subgraph_counts = count_unique_subgraphs(graph, max_graphlet_size)
+        sorted_sg = dict(sorted(subgraph_counts.items()))
+        pred_df4["g2_1"] = subgraph_counts["g2"]["g2_1"]
+        pred_df4 = pred_df4.loc[:, graphlet_names[1:]+["g2_1"]]
+        # Extract the values in the desired order
+        values = []
+        for key in sorted_sg.keys():
+            sub_dict = sorted_sg[key]
+            for sub_key in sorted(sub_dict):
+                values.append(sub_dict[sub_key])
+        
+        graphlets = np.array(values).reshape(-1, )
+        loc = classes.index(clas)
+        feats[clas] += [np.array(temp_df)]
+        connected_subgraphs[clas] += [graphlets]
+        
+        lists_of_full = []
+        # Convert the list of values to a numpy array with shape (9, 1)
+        for key in feats.keys():
+            sublist_descriptors = [arr[:,:-1] for arr in feats[key]]
+            sublist_add_features = connected_subgraphs[key]
+            lists_of_full.append(process_sublist(sublist_descriptors, sublist_add_features, kmeans))
+        
+        # Convert the list of values to a numpy array with shape (9, 1)
+        # pred_df3 = 
+        pred_df3 = lists_of_full[1][0] if len(lists_of_full[1])!=0 else lists_of_full[0][0]
+        yhat = model.predict(pred_df3.reshape(1,-1))
+        plt.figure()
+        plt.imshow(img, 'gray')
+        plt.imshow(
+            coefs_plane, alpha=0.5,
+            cmap = 'bwr',
+            vmin=-np.max(np.abs(model.coef_)), 
+            vmax=np.max(np.abs(model.coef_))
+        )
+        plt.colorbar()
+        plt.axis("off")
+        plt.show()
+        plt.savefig(f"{results_location}/{img_name_only}_heatmap.png")
+
+        print(f"Predicted class: {classes[yhat[0]]}({yhat[0]})")
+        print(f"Actual class : {clas}")
+# %%
